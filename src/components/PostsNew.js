d1184a65cd256cbdd08f3558a091c284d503d06d
zhulinpinyu <mulixiang199016@gmail.com>
2016-05-04 12:15:22 +0800

    complete field validate and add cancel btn

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {reduxForm} from 'redux-form'
+import {Link} from 'react-router'
 
 import {createPost} from '../actions/index'
 
@@ -10,22 +11,29 @@ class PostsNew extends Component{
     return (
       <form onSubmit={handleSubmit(createPost)}>
         <h3>Create a New Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
-          <div className="text-danger">
+          <div className="text-help">
             {title.touched ? title.error : ''}
           </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories}/>
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea className="form-control" {...content}/>
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
@@ -35,6 +43,12 @@ const validate = (values) => {
   const errors = {}
   if(!values.title){
     errors.title = "Pls input title."
+  }
+  if(!values.categories){
+    errors.categories = "Pls input categories."
+  }
+  if(!values.content){
+    errors.content = "Pls input content."
   }
   return errors
 }
